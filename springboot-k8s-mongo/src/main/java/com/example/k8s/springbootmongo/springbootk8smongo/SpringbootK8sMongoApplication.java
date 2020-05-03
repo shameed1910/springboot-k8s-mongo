7fd2dc1f2e82998e6b740589a94603238fe2162a
hameed <hameed@slave-node>
2020-05-03 16:49:13 +0530

    spring cloud with mongo in kubernetes

---
+++ springboot-k8s-mongo/src/main/java/com/example/k8s/springbootmongo/springbootk8smongo/SpringbootK8sMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8s.springbootmongo.springbootk8smongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8sMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sMongoApplication.class, args);
+	}
+
+}
